bb0c9d8e0923807eb811ced1fb93aea421d312cf
nanoDW <s.fluder@interia.eu>
2019-05-18 12:30:45 +0200

    Add !markdown command

---
+++ src/commands/markdown.ts
@@ -0,0 +1,23 @@
+import Discord from 'discord.js';
+import { Command } from '../types';
+
+const regulamin: Command = {
+    name: 'markdown',
+    description: 'Wyświetla przykłady formatowania tekstu w Markdown.',
+    args: false,
+    execute(msg: Discord.Message) {
+        return msg.channel.send('*kursywa* or _kursywa_ \n'
+            + '__ * podkreślona kursywa * __ \n'
+            + '** pogrubiony ** \n'
+            + '__ ** podkreślony pogrubiony ** __ \n'
+            + '*** pogrubiona kursywa *** \n'
+            + '__ *** podkreślona pogrubiona kursywa *** __ \n'
+            + '__podkreślenie__ \n'
+            + '~~przekreślenie~~ \n'
+            + '```js (or html, css, json, cp, md, py, xml etc.) \n'
+            + 'kod \n'
+            + '``` \n');
+    },
+};
+
+export default regulamin;
