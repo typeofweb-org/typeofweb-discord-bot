e2d8614e736866347498dc16b1ce660b033272d3
nanoDW <s.fluder@interia.eu>
2019-05-18 12:37:09 +0200

    Fix formatting and name of the command

--- src/commands/markdown.ts
+++ src/commands/markdown.ts
@@ -1,23 +1,23 @@
 import Discord from 'discord.js';
 import { Command } from '../types';
 
-const regulamin: Command = {
+const markdown: Command = {
     name: 'markdown',
     description: 'Wyświetla przykłady formatowania tekstu w Markdown.',
     args: false,
     execute(msg: Discord.Message) {
-        return msg.channel.send('*kursywa* or _kursywa_ \n'
+        return msg.channel.send('```*kursywa* or _kursywa_ \n'
             + '__ * podkreślona kursywa * __ \n'
             + '** pogrubiony ** \n'
             + '__ ** podkreślony pogrubiony ** __ \n'
             + '*** pogrubiona kursywa *** \n'
             + '__ *** podkreślona pogrubiona kursywa *** __ \n'
             + '__podkreślenie__ \n'
-            + '~~przekreślenie~~ \n'
-            + '```js (or html, css, json, cp, md, py, xml etc.) \n'
+            + '~~przekreślenie~~``` \n'
+            + '\```js (or html, css, json, cp, md, py, xml etc.) \n'
             + 'kod \n'
-            + '``` \n');
+            + '\``` \n');
     },
 };
 
-export default regulamin;
+export default markdown;
