2d19aca922128640c08509d992b1825f7144d9be
larto <94konrad.n@gmail.com>
2020-01-28 19:06:33 +0100

    Add 'odpowiedz' command (#37)
    * Add 'odpowiedz' command
    
    * Change implementation of randomizeArray function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,8 @@
-export function randomizeArray<T>(array: T[]) {
-  const randomPivot = 0.5;
-  return [...array].sort(() => randomPivot - Math.random());
+export function randomizeArray<T>(arr: T[]) {
+  const array = [...arr];
+  for (let mutableIndex = 0; mutableIndex < array.length; ++mutableIndex) {
+    const j = Math.floor(Math.random() * (mutableIndex + 1));
+    [array[mutableIndex], array[j]] = [array[j], array[mutableIndex]];
+  }
+  return array;
 }
