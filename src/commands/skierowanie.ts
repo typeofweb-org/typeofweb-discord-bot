7e66f8bca1b0b7ebf3926c943866322f6f5b52c4
Adam Siekierski <a@siekierski.ml>
2020-12-16 10:19:33 +0100

    Skierowanie improvments - better links, bug fixes

--- src/commands/skierowanie.ts
+++ src/commands/skierowanie.ts
@@ -17,6 +17,9 @@ const skierowanie: Command = {
   args: true,
   cooldown: 10,
   async execute(msg, args) {
+    // Filter out the empty args (whitespaces, etc)
+    args = args.filter((val) => val);
+
     const skierowanieEmbed = new Discord.RichEmbed()
       .setColor('#5ab783')
       .setAuthor(
@@ -24,15 +27,15 @@ const skierowanie: Command = {
         msg.author.avatarURL,
         'https://typeofweb.com'
       )
-      .setTitle('Skierowanie na naukę podstaw')
+      .setTitle('Skierowanie na naukę podstaw \uD83D\uDE91')
       .setThumbnail('https://typeofweb.com/wp-content/uploads/2020/04/logo_kwadrat11.png')
       .addField(
         'Działając na podstawie mojej intuicji oraz wiadomości wysłanych przez osobę skierowaną, kieruję użytkownika/użytkowniczkę',
-        args[0]
+        `${args[0]},`
       )
       .addField(
-        `na naukę podstaw wybranej przez siebie technologii, w celu lepszego zrozumienia fundamentów jej działania oraz poznania informacji niezbędnych do rozszerzania swojej wiedzy o bardziej zaawansowane zagadnienia`,
-        `Obecnie posiadana wiedza przez wyżej wymienioną osobę jest zbyt wąska, by mogła ona dalej kontynuować swoją naukę w trudniejszych zagadnieniach w sposób efektywny i zrozumiały`
+        `na naukę podstaw wybranej przez siebie technologii,`,
+        `w celu lepszego zrozumienia fundamentów jej działania oraz poznania informacji niezbędnych do rozszerzania swojej wiedzy o bardziej zaawansowane zagadnienia`
       )
       .setTimestamp()
       .setFooter(
@@ -43,12 +46,17 @@ const skierowanie: Command = {
     const categoryFilter = args[1]?.toLowerCase();
     const linksFiltered = categoryFilter
       ? links.filter(({ category }) => !category || category === categoryFilter)
-      : links;
+      : links.filter(({ category }) => !category);
 
     const linksMessage = 'Z powyższym skierowaniem należy udać się na poniższe strony internetowe:';
 
+    const linksEmbed = new Discord.RichEmbed().addField(
+      linksMessage,
+      linksFiltered.map((l) => l.url).join('\n')
+    );
+
     await msg.channel.send(skierowanieEmbed);
-    return msg.channel.send([linksMessage, ...linksFiltered.map((link) => link.url)]);
+    return msg.channel.send(linksEmbed);
   },
 };
 
