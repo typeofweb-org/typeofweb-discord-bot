df05d3a11cd25aa2efe985637a3a40550a79842d
Michal Miszczyszyn <michal@mmiszy.pl>
2022-01-11 16:04:28 +0100

    fix: Fix typeofweb command

--- src/commands/towarticle.ts
+++ src/commands/towarticle.ts
@@ -1,57 +1,78 @@
 import { Command } from '../types';
-import fetch from 'node-fetch';
 import { polishPlurals } from 'polish-plurals';
+import { getConfig } from '../config';
+import Algoliasearch from 'algoliasearch';
+import { MessageEmbed } from 'discord.js';
 
 const pluralize = (count: number) => polishPlurals('artykuł', 'artykuły', 'artykułów', count);
+
+const PER_PAGE = 3;
 
 const typeofweb: Command = {
   name: 'typeofweb',
   description: 'Wyszukuje artykuły z Type of Web',
   args: true,
   async execute(msg, args) {
-    const query = encodeURIComponent(args.join(' '));
-    const res = await fetch(
-      `https://typeofweb.com/wp-json/wp/v2/search?search=${query}&per_page=3`,
-    );
+    const ALGOLIA_APP_ID = getConfig('ALGOLIA_APP_ID');
+    const ALGOLIA_API_KEY = getConfig('ALGOLIA_API_KEY');
+    const ALGOLIA_INDEX_NAME = getConfig('ALGOLIA_INDEX_NAME');
 
-    const data = (await res.json()) as ToWSearchResponse[];
-    if (!data.length) {
+    const client = Algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+
+    const query = args.join(' ');
+    const res = await index.search<TypeOfWebAlgoliaResult>(query, {
+      hitsPerPage: PER_PAGE,
+    });
+    const total = res.hits.length;
+
+    if (!total) {
       return msg.channel.send(`Niestety nic nie znalazłam 😭`);
     }
-    const total = data.length;
-    const message = `Pokazuję pierwsze ${total} ${pluralize(total)}` + ':';
 
-    const article = data.slice(0, total);
-    return msg.channel.send([message, ...article.map((doc) => doc.url)]);
+    console.log(res.hits.map((h) => h.authors));
+
+    const results = res.hits.map((h) =>
+      new MessageEmbed()
+        .setTitle(h.title)
+        .setURL(`https://typeofweb.com/${h.permalink}`)
+        .setThumbnail(`https://typeofweb.com/${h.img.url.replace(/^\/public\//, '')}`)
+        .setFooter(`https://typeofweb.com/${h.permalink}`)
+        .setColor([28, 160, 86])
+        .setAuthor(
+          h.authors.map((a) => a.replace(/(?:^|-)(\w)/g, (_, m) => ' ' + m.toUpperCase()).trim()),
+        )
+        .setDescription(h.excerpt),
+    );
+
+    return results.reduce(async (acc, embed) => {
+      await acc;
+      return msg.channel.send({ embed });
+    }, msg.channel.send(`Pokazuję pierwsze ${total} ${pluralize(total)}:`));
   },
 };
 
 export default typeofweb;
 
-interface ToWSearchResponse {
-  id: number;
+interface TypeOfWebAlgoliaResult {
   title: string;
-  url: string;
+  date: Date;
   type: string;
-  subtype: string;
-  _links: Links;
+  permalink: string;
+  authors: string[];
+  excerpt: string;
+  content: string;
+  img: Img;
+  category: Category;
 }
 
-interface Links {
-  self: Self[];
-  about: About[];
-  collection: Collection[];
+interface Category {
+  slug: string;
+  name: string;
 }
 
-interface Self {
-  embeddable: boolean;
-  href: string;
-}
-
-interface About {
-  href: string;
-}
-
-interface Collection {
-  href: string;
+interface Img {
+  url: string;
+  width: number;
+  height: number;
 }
