45a16f14a1203fcd035e2de5b8f3fe67be663b35
Bartek532 <bartzagr@gmail.com>
2022-01-18 20:40:47 +0100

    fix help - add syntax in description to skierowanie command

--- src/commands/skierowanie.ts
+++ src/commands/skierowanie.ts
@@ -33,7 +33,7 @@ const links = [
 
 const skierowanie: Command = {
   name: 'skierowanie',
-  description: 'Skierowanie na naukę podstaw (+ dobre materiały do nauki)',
+  description: 'Skierowanie na naukę podstaw (+ dobre materiały do nauki). Składnia: `!skierowanie <uzytkownik> <technologia>`',
   args: 'required',
   cooldown: 10,
   async execute(msg, av) {
