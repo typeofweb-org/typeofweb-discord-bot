ae322bceeeaa8f840f783ff11ce1dfdb5af23453
Michal Michalczuk <michalczukm@gmail.com>
2022-01-26 00:09:22 +0100

    fix: youtube command

--- src/commands/youtube.ts
+++ src/commands/youtube.ts
@@ -1,6 +1,10 @@
-import { getConfig } from '../config';
-import { Command } from '../types';
 import fetch from 'node-fetch';
+
+import { getConfig } from '../config';
+import type { Command } from '../types';
+
+const isErrorResponse = (response: YoutubeResponse): response is YoutubeResponseError =>
+  'error' in response;
 
 const youtube: Command = {
   name: 'youtube',
@@ -12,39 +16,53 @@ const youtube: Command = {
     const result = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=id&type=video&key=${YOUTUBE_API_KEY}&q=${query}`,
     );
-    const data = (await result.json()) as YoutubeResponse;
+    const response = (await result.json()) as YoutubeResponse;
 
-    if (!data.items.length) {
+    if (isErrorResponse(response)) {
+      return msg.channel.send(`Uh, integracja z API YT nie zadziałała 😭`);
+    }
+
+    if (!response.items.length) {
       return msg.channel.send(`Niestety nic nie znalazłam 😭`);
     }
 
-    const { videoId } = data.items[0].id;
+    const [
+      {
+        id: { videoId },
+      },
+    ] = response.items;
     return msg.channel.send(`https://www.youtube.com/watch?v=${videoId}`);
   },
 };
 
 export default youtube;
 
-interface YoutubeResponse {
-  kind: string;
-  etag: string;
-  nextPageToken: string;
-  regionCode: string;
-  pageInfo: PageInfo;
-  items: YoutubeItem[];
+interface YoutubeResponseError {
+  readonly error: unknown;
+}
+
+interface YoutubeResponseSuccess {
+  readonly kind: string;
+  readonly etag: string;
+  readonly nextPageToken: string;
+  readonly regionCode: string;
+  readonly pageInfo: PageInfo;
+  readonly items: ReadonlyArray<YoutubeItem>;
 }
 
 interface PageInfo {
-  totalResults: number;
-  resultsPerPage: number;
+  readonly totalResults: number;
+  readonly resultsPerPage: number;
 }
 
 interface YoutubeItem {
-  kind: string;
-  etag: string;
-  id: YoutubeItemId;
+  readonly kind: string;
+  readonly etag: string;
+  readonly id: YoutubeItemId;
 }
 interface YoutubeItemId {
-  kind: string;
-  videoId: string;
+  readonly kind: string;
+  readonly videoId: string;
 }
+
+type YoutubeResponse = YoutubeResponseSuccess | YoutubeResponseError;
